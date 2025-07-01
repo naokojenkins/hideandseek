fbdac7577ec1dd7f393ad84d304ec40708236e9e
Гончаров Алексей Викторович <al.v.goncharov@tinkoff.ru>
2025-07-01 11:37:06 +0300

    4

--- ToolUse.Sim/obj/Debug/net9.0/ToolUse.Sim.AssemblyInfo.cs
+++ ToolUse.Sim/obj/Debug/net9.0/ToolUse.Sim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToolUse.Sim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aa59472bf4cc0ad1487f3e1030f4218ecf38d8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f6237a84e21c5b586b6701aacf7c0f1df36739a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToolUse.Sim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToolUse.Sim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
